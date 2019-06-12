f4495de38a4be10d666a545923fcee5af80126f0
Fabricio Bracht <fabracht@gmail.com>
2019-06-11 19:03:01 -0700

    Initial Commit

---
+++ neuralnet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neuralnet
+//
+//  Created by Fabricio Bracht on 2019-06-11.
+//  Copyright © 2019 Fabricio Bracht. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
